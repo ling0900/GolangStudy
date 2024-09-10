aa568c9eaf8a38883e7b123338e4e8ec1722ba61
lh <992610900@qq.com>
2024-09-10 23:36:13 +0800

    指针、局部变量。

--- 6-pointer/pointer.go
+++ 6-pointer/pointer.go
@@ -2,17 +2,21 @@ package main
 
 import "fmt"
 
-/*
+
 func swap(a int ,b int) {
+	fmt.Println("a和b内部交换前：a = ", a, " b = ", b)
 	var temp int
 	temp = a
 	a = b
 	b = temp
+	fmt.Println("a和b内部交换后：a = ", a, " b = ", b)
 }
-*/
 
-func swap(pa *int, pb *int) {
+
+func swap2(pa *int, pb *int) {
+	// 定义一个中间变量
 	var temp int
+	// 交换
 	temp = *pa //temp = main::a
 	*pa = *pb  // main::a = main::b
 	*pb = temp // main::b = temp
@@ -23,22 +27,33 @@ func main() {
 	var a int = 10
 	var b int = 20
 
-	swap(&a, &b)
+	fmt.Println("a和b交换前：a = ", a, " b = ", b)
 
-	fmt.Println("a = ", a, " b = ", b)
+	fmt.Println("a和b交换前的地址：a = ", &a, " b = ", &b)
 
+	swap(a, b)
 
+	fmt.Println("a和b交换后：a = ", a, " b = ", b)
+	fmt.Println("a和b交换后的地址：a = ", &a, " b = ", &b)
+
+	// 定义一个指针变量 p
 	var p *int
 
+	// 指针变量的存储地址到p
 	p = &a
 
+	// 打印p的地址
 	fmt.Println(&a)
+	// 打印p的值
 	fmt.Println(p)
 
-	var pp **int //二级指针
+	// 定义一个二级指针变量 pp
+	var pp **int
 
 	pp = &p
 
+	// 打印pp的地址
 	fmt.Println(&p)
+	// 打印pp的值
 	fmt.Println(pp)
 }
